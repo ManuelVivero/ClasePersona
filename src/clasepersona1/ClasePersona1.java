c4fb8619a4cc36211f4fd6410898a3c1bedfe087
ManuelVivero <1776381@alu.murciaeduca.es>
2023-05-09 16:01:33 +0200

    inicio proyecto

---
+++ src/clasepersona1/ClasePersona1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package clasepersona1;
+
+/**
+ *
+ * @author 17763
+ */
+public class ClasePersona1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
