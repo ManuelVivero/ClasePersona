6cb10d3659c87666cfdb4bf4d6d8d7c04e515718
ManuelVivero <1776381@alu.murciaeduca.es>
2023-05-09 16:10:24 +0200

    ejercicio 3 juezLTI

--- src/clasepersona1/ClasePersona1.java
+++ src/clasepersona1/ClasePersona1.java
@@ -11,7 +11,7 @@ public class ClasePersona1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Commit 3");
+        System.out.println("Commit 4");
     }
     
 }
